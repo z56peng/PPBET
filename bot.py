07e2ec88af23456adcc1e49c34aae83e752c8b56
z56peng <z56peng@uwaterloo.ca>
2024-02-27 11:38:35 -0500

    CMD sync adjustment

--- bot.py
+++ bot.py
@@ -10,21 +10,20 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
-tree = app_commands.CommandTree(bot)
 
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user.name}')
     db.initialize_db()  # 初始化数据库
-    await tree.sync(guild=discord.Object(id=1211882878699311144))
+    await bot.tree.sync(guild=discord.Object(id=1211882878699311144))
 
-@tree.hybrid_command()
+@bot.hybrid_command()
 async def balance(ctx):
     user_id = ctx.author.id
     user_balance = db.get_balance(user_id)
     await ctx.send(f'Your balance is {user_balance} PP coins.')
 
-@tree.hybrid_command()
+@bot.hybrid_command()
 async def add(ctx, fund: int):
     user_id = ctx.author.id
     user_balance = db.get_balance(user_id)
